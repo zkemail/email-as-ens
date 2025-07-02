6fb2b2f3ea4e946563acb9dbcc167729f3c7de41
Bence Háromi <56651250+benceharomi@users.noreply.github.com>
2025-07-02 18:25:37 +0100

    docs: missing natspec for IGroth16Verifier

--- src/utils/Verifier.sol
+++ src/utils/Verifier.sol
@@ -48,6 +48,16 @@ struct ProveAndClaimCommand {
 }
 
 interface IGroth16Verifier {
+    /**
+     * @notice Verifies a Groth16 zero-knowledge proof
+     * @param _pA The first component of the proof (A point)
+     * @param _pB The second component of the proof (B point)
+     * @param _pC The third component of the proof (C point)
+     * @param _pubSignals The public signals used in the proof verification
+     * @return True if the proof is valid, false otherwise
+     * @dev This function verifies a Groth16 zk-SNARK proof by checking that the proof
+     *      satisfies the circuit constraints defined by the public signals.
+     */
     function verifyProof(
         uint256[2] calldata _pA,
         uint256[2][2] calldata _pB,
