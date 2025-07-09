35e4ac11c3d37a5b809156ea5fe66123da1ff186
Bence Háromi <56651250+benceharomi@users.noreply.github.com>
2025-07-09 14:45:21 +0100

    chore: moved IGroth16Verifier to its own file

--- src/utils/Verifier.sol
+++ src/utils/Verifier.sol
@@ -5,6 +5,7 @@ import { CommandUtils } from "@zk-email/email-tx-builder/src/libraries/CommandUt
 import { Bytes } from "@openzeppelin/contracts/utils/Bytes.sol";
 import { Strings } from "@openzeppelin/contracts/utils/Strings.sol";
 import { CircuitUtils } from "./CircuitUtils.sol";
+import { IGroth16Verifier } from "../interfaces/IGroth16Verifier.sol";
 
 /**
  * @title ProveAndClaimCommand
@@ -47,28 +48,6 @@ struct ProveAndClaimCommand {
     /// @notice The zero-knowledge proof that validates all fields in this struct
     /// @dev Contains the proof compatible with verifier
     bytes proof;
-}
-
-interface IGroth16Verifier {
-    /**
-     * @notice Verifies a Groth16 zero-knowledge proof
-     * @param _pA The first component of the proof (A point)
-     * @param _pB The second component of the proof (B point)
-     * @param _pC The third component of the proof (C point)
-     * @param _pubSignals The public signals used in the proof verification
-     * @return True if the proof is valid, false otherwise
-     * @dev This function verifies a Groth16 zk-SNARK proof by checking that the proof
-     *      satisfies the circuit constraints defined by the public signals.
-     */
-    function verifyProof(
-        uint256[2] calldata _pA,
-        uint256[2][2] calldata _pB,
-        uint256[2] calldata _pC,
-        uint256[60] calldata _pubSignals
-    )
-        external
-        view
-        returns (bool);
 }
 
 /**
