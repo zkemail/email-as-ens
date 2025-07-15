2de8d5025a663e41ea5c4ebf2ad5c284515f1b53
zkfriendly <zkfriendly@proton.me>
2025-07-15 13:28:06 +0200

    refactor: claim with resolver

--- script/ClaimWithFixtureCommand.sol
+++ script/ClaimWithFixtureCommand.sol
@@ -14,8 +14,6 @@ import { ProveAndClaimCommand } from "../src/utils/Verifier.sol";
  *      with the deployed ZkEmailRegistrar contract
  */
 contract ClaimWithFixtureCommandScript is Script {
-    address public constant PUBLIC_RESOLVER = 0x8948458626811dd0c23EB25Cc74291247077cC51;
-
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         address zkEmailRegistrarAddress = vm.envAddress("ZK_EMAIL_REGISTRAR");
@@ -24,7 +22,7 @@ contract ClaimWithFixtureCommandScript is Script {
         console.log("ZkEmailRegistrar address:", zkEmailRegistrarAddress);
 
         // Get the sample fixture command
-        (ProveAndClaimCommand memory command,) = TestFixtures.claimEnsCommand();
+        (ProveAndClaimCommand memory command,) = TestFixtures.claimEnsCommandWithResolver();
 
         vm.startBroadcast(deployerPrivateKey);
 
@@ -33,14 +31,6 @@ contract ClaimWithFixtureCommandScript is Script {
 
         try registrar.entrypoint(abi.encode(command)) {
             console.log("Successfully claimed ENS name!");
-            bytes32 node = 0xe732be81ce46c5f5caddad0003bac9aa8fe88e5c22eaf2576470f380b975df38;
-            try registrar.setRecord(node, command.owner, PUBLIC_RESOLVER, 0) {
-                console.log("Successfully set record!");
-            } catch Error(string memory reason) {
-                console.log("Set record failed with reason:", reason);
-            } catch {
-                console.log("Set record failed with unknown error");
-            }
         } catch Error(string memory reason) {
             console.log("Claim failed with reason:", reason);
         } catch {
