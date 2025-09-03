ac325f38d58303ca89e2535a200fc8e2d92d4893
Bence Háromi <56651250+benceharomi@users.noreply.github.com>
2025-09-03 20:46:58 +0100

    feat: link x command verifier reworked

--- src/verifiers/LinkXCommandVerifier.sol
+++ src/verifiers/LinkXCommandVerifier.sol
@@ -1,46 +1,47 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
 
-import { console } from "forge-std/console.sol";
-import { CircuitUtils } from "@zk-email/contracts/CircuitUtils.sol";
 import { IHonkVerifier } from "../interfaces/IHonkVerifier.sol";
+import { NoirUtils, BoundedVec, Field } from "../utils/NoirUtils.sol";
+import { Strings } from "@openzeppelin/contracts/utils/Strings.sol";
 
-struct XProof {
-    XDecodedFields fields;
-    bytes proof;
+struct PubSignals {
+    Field pubkeyHash;
+    Field headerHash0;
+    Field headerHash1;
+    Field[] proverAddress;
+    Field[] owner;
+    BoundedVec xHandleCapture1;
 }
 
-struct XDecodedFields {
-    bytes32 pubkeyHash;
-    bytes32 headerHash0;
-    bytes32 headerHash1;
-    string proverAddress;
-    string owner;
-    string xHandleCapture1;
-}
-
-struct LinkXCommand {
+struct Command {
     string xHandle;
     string ensName;
-    XProof proof;
+    bytes proof;
+    PubSignals pubSignals;
 }
 
 contract LinkXCommandVerifier {
     // #1: pubkey_hash 32 bytes -> 1 field -> idx 0
     uint256 public constant PUBKEY_HASH_OFFSET = 0;
+    uint256 public constant PUBKEY_HASH_LEN = 1;
     // #2: header_hash_0 32 bytes -> 1 field -> idx 1
     uint256 public constant HEADER_HASH_0_OFFSET = 1;
+    uint256 public constant HEADER_HASH_0_LEN = 1;
     // #3: header_hash_1 32 bytes -> 1 field -> idx 2
     uint256 public constant HEADER_HASH_1_OFFSET = 2;
+    uint256 public constant HEADER_HASH_1_LEN = 1;
     // #4: prover_address CEIL(31 bytes / 31 bytes per field) = 1 field -> idx 3
     uint256 public constant PROVER_ADDRESS_OFFSET = 3;
-    uint256 public constant PROVER_ADDRESS_SIZE = 31;
+    uint256 public constant PROVER_ADDRESS_LEN = 1;
     // #5: owner CEIL(93 bytes / 31 bytes per field) = 3 fields -> idx 4-6
     uint256 public constant OWNER_OFFSET = 4;
-    uint256 public constant OWNER_SIZE = 93; // 3 * 31 = 93 ????
+    uint256 public constant OWNER_LEN = 3;
     // // #6: x_handle_capture 64 fields + 1 field (length) = 65 fields -> idx 7-71
     uint256 public constant X_HANDLE_CAPTURE_1_OFFSET = 7;
-    uint256 public constant X_HANDLE_CAPTURE_1_SIZE = 2015; // length 64 * 31 = 1984 + 31 = 2015 ????
+    uint256 public constant X_HANDLE_CAPTURE_1_MAX_LEN = 64;
+
+    uint256 public constant PUBLIC_SIGNALS_LENGTH = 72;
 
     address public immutable HONK_VERIFIER;
 
@@ -49,89 +50,69 @@ contract LinkXCommandVerifier {
     }
 
     function verify(bytes memory data) external view returns (bool) {
-        return _isValid(abi.decode(data, (LinkXCommand)));
+        Command memory command = abi.decode(data, (Command));
+
+        return IHonkVerifier(HONK_VERIFIER).verify(command.proof, _encodePubSignals(command.pubSignals))
+            && Strings.equal(command.ensName, command.ensName)
+            && _compareXHandle(command.xHandle, command.pubSignals.xHandleCapture1);
     }
 
     function encode(
-        uint256[] calldata pubSignals,
-        bytes calldata proof
+        bytes calldata proof,
+        bytes32[] calldata pubSignals
     )
         external
         pure
         returns (bytes memory encodedCommand)
     {
-        return abi.encode(_buildLinkXCommand(pubSignals, proof));
+        return abi.encode(_buildCommand(pubSignals, proof));
     }
 
-    function _isValid(LinkXCommand memory command) internal view returns (bool) {
-        return _verifyXProof(command.proof, HONK_VERIFIER);
+    function _encodePubSignals(PubSignals memory decodedFields) internal pure returns (bytes32[] memory pubSignals) {
+        bytes32[][] memory fields = new bytes32[][](6);
+        fields[0] = NoirUtils.encodeField(decodedFields.pubkeyHash);
+        fields[1] = NoirUtils.encodeField(decodedFields.headerHash0);
+        fields[2] = NoirUtils.encodeField(decodedFields.headerHash1);
+        fields[3] = NoirUtils.encodeFieldArray(decodedFields.proverAddress);
+        fields[4] = NoirUtils.encodeFieldArray(decodedFields.owner);
+        fields[5] = NoirUtils.encodeBoundedVec(decodedFields.xHandleCapture1);
+
+        return NoirUtils.flattenArray(fields, PUBLIC_SIGNALS_LENGTH);
     }
 
-    function _verifyXProof(XProof memory xProof, address honkVerifier) internal view returns (bool isValid) {
-        uint256[72] memory pubSignalsUint256 = _packPubSignals(xProof.fields);
+    function _decodePubSignals(bytes32[] calldata encoded) internal pure returns (PubSignals memory decoded) {
+        if (encoded.length != PUBLIC_SIGNALS_LENGTH) revert NoirUtils.InvalidPubSignalsLength();
 
-        bytes32[] memory pubSignals = new bytes32[](72);
-        for (uint256 i = 0; i < 72; i++) {
-            pubSignals[i] = bytes32(pubSignalsUint256[i]);
-        }
-
-        // verify the proof
-        bool validProof = IHonkVerifier(honkVerifier).verify(xProof.proof, pubSignals);
-
-        return validProof;
+        return PubSignals({
+            pubkeyHash: NoirUtils.decodeField(encoded, PUBKEY_HASH_OFFSET),
+            headerHash0: NoirUtils.decodeField(encoded, HEADER_HASH_0_OFFSET),
+            headerHash1: NoirUtils.decodeField(encoded, HEADER_HASH_1_OFFSET),
+            proverAddress: NoirUtils.decodeFieldArray(encoded, PROVER_ADDRESS_OFFSET, PROVER_ADDRESS_LEN),
+            owner: NoirUtils.decodeFieldArray(encoded, OWNER_OFFSET, OWNER_LEN),
+            xHandleCapture1: NoirUtils.decodeBoundedVec(encoded, X_HANDLE_CAPTURE_1_OFFSET, X_HANDLE_CAPTURE_1_MAX_LEN)
+        });
     }
 
-    function _packPubSignals(XDecodedFields memory decodedFields)
-        internal
-        pure
-        returns (uint256[72] memory pubSignals)
-    {
-        uint256[][] memory fields = new uint256[][](6);
-        fields[0] = CircuitUtils.packBytes32(decodedFields.pubkeyHash);
-        fields[1] = CircuitUtils.packBytes32(decodedFields.headerHash0);
-        fields[2] = CircuitUtils.packBytes32(decodedFields.headerHash1);
-        fields[3] = CircuitUtils.packString(decodedFields.proverAddress, PROVER_ADDRESS_SIZE);
-        fields[4] = CircuitUtils.packString(decodedFields.owner, OWNER_SIZE);
-        fields[5] = CircuitUtils.packString(decodedFields.xHandleCapture1, X_HANDLE_CAPTURE_1_SIZE);
-
-        uint256[] memory _pubSignals = CircuitUtils.flattenFields(fields, pubSignals.length);
-        for (uint256 i = 0; i < _pubSignals.length; i++) {
-            pubSignals[i] = _pubSignals[i];
-        }
-
-        return pubSignals;
-    }
-
-    function _unpackPubSignals(uint256[] calldata pubSignals)
-        internal
-        pure
-        returns (XDecodedFields memory decodedFields)
-    {
-        console.log("pubSignals.length", pubSignals.length);
-
-        if (pubSignals.length != 72) revert CircuitUtils.InvalidPubSignalsLength();
-
-        decodedFields.pubkeyHash = CircuitUtils.unpackBytes32(pubSignals, PUBKEY_HASH_OFFSET);
-        decodedFields.headerHash0 = CircuitUtils.unpackBytes32(pubSignals, HEADER_HASH_0_OFFSET);
-        decodedFields.headerHash1 = CircuitUtils.unpackBytes32(pubSignals, HEADER_HASH_1_OFFSET);
-        decodedFields.proverAddress = CircuitUtils.unpackString(pubSignals, PROVER_ADDRESS_OFFSET, PROVER_ADDRESS_SIZE);
-        decodedFields.owner = CircuitUtils.unpackString(pubSignals, OWNER_OFFSET, OWNER_SIZE);
-        decodedFields.xHandleCapture1 =
-            CircuitUtils.unpackString(pubSignals, X_HANDLE_CAPTURE_1_OFFSET, X_HANDLE_CAPTURE_1_SIZE);
-
-        return decodedFields;
-    }
-
-    function _buildLinkXCommand(
-        uint256[] calldata pubSignals,
+    function _buildCommand(
+        bytes32[] calldata encodedPubSignals,
         bytes memory proof
     )
         private
         pure
-        returns (LinkXCommand memory command)
+        returns (Command memory command)
     {
-        XDecodedFields memory decodedFields = _unpackPubSignals(pubSignals);
+        PubSignals memory pubSignals = _decodePubSignals(encodedPubSignals);
+        return Command({
+            xHandle: string(abi.encodePacked(pubSignals.xHandleCapture1.elements)),
+            ensName: "zkfriendly.eth",
+            proof: proof,
+            pubSignals: pubSignals
+        });
+    }
 
-        return LinkXCommand({ xHandle: "", ensName: "", proof: XProof({ fields: decodedFields, proof: proof }) });
+    function _compareXHandle(string memory xHandle, BoundedVec memory xHandleCapture1) private pure returns (bool) {
+        string memory xHandleString = string(abi.encodePacked(xHandleCapture1.elements));
+        bool res = Strings.equal(xHandle, xHandleString);
+        return res;
     }
 }
