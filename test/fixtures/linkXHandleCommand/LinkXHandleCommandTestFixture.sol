b9670f20bd8e0e2a508ca4e5956bbf3a237c1e95
Bence Háromi <56651250+benceharomi@users.noreply.github.com>
2025-09-22 15:51:33 +0100

    feat: _getProof fixture helper added to read raw proof

--- test/fixtures/linkXHandleCommand/LinkXHandleCommandTestFixture.sol
+++ test/fixtures/linkXHandleCommand/LinkXHandleCommandTestFixture.sol
@@ -29,6 +29,11 @@ library LinkXHandleCommandTestFixture {
         return (command, _getPublicInputsFields(path));
     }
 
+    /**
+     * @notice Reads the expected pub signals from a `expected_pub_signals.json` file (PubSignals struct object)
+     * @param dirPath Path to the directory containing the `expected_pub_signals.json` file
+     * @return pubSignals The PubSignals struct object
+     */
     function _getExpectedPubSignals(string memory dirPath) private view returns (PubSignals memory pubSignals) {
         string memory pubSignalsFile = vm.readFile(string.concat(dirPath, "expected_pub_signals.json"));
         return PubSignals({
@@ -41,11 +46,43 @@ library LinkXHandleCommandTestFixture {
         });
     }
 
+    /**
+     * @notice Reads the proof from a `proof_fields.json` file (array of field / bytes32 values)
+     * @param dirPath Path to the directory containing the `proof_fields.json` file
+     * @return proofFields The proof fields
+     */
     function _getProofFields(string memory dirPath) private view returns (bytes32[] memory proofFields) {
         bytes memory proofFieldsData = vm.parseJson(vm.readFile(string.concat(dirPath, "proof_fields.json")), ".");
         return abi.decode(proofFieldsData, (bytes32[]));
     }
 
+    /**
+     * @notice Reads the proof from a `proof` file (raw bytes of the proof)
+     * @param dirPath Path to the directory containing the `proof` file
+     * @return proofFields The proof fields
+     */
+    function _getProof(string memory dirPath) private view returns (bytes32[] memory proofFields) {
+        // 1) Read the raw bytes
+        bytes memory packed = vm.readFileBinary(string.concat(dirPath, "proof"));
+
+        // 2) Decode the blob into fixed bytes32[440]
+        (bytes32[440] memory proofFixed) = abi.decode(packed, (bytes32[440]));
+
+        // 3) Convert to dynamic bytes32[]
+        proofFields = new bytes32[](440);
+        for (uint256 i = 0; i < 440; i++) {
+            proofFields[i] = proofFixed[i];
+        }
+
+        // 4) Return the proof
+        return proofFields;
+    }
+
+    /**
+     * @notice Reads the public inputs from a `public_inputs_fields.json` file (array of field / bytes32 values)
+     * @param dirPath Path to the directory containing the `public_inputs_fields.json` file
+     * @return publicInputs The public inputs fields
+     */
     function _getPublicInputsFields(string memory dirPath) private view returns (bytes32[] memory publicInputs) {
         bytes memory publicInputsFieldsData =
             vm.parseJson(vm.readFile(string.concat(dirPath, "public_inputs_fields.json")), ".");
