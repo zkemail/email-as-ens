5df714afd382a514d7a19b9b6cdbff7c44497245
Bence Háromi <56651250+benceharomi@users.noreply.github.com>
2025-10-08 13:05:27 +0100

    test: expect revert instead of false for invalid proof

--- test/src/verifiers/LinkXHandleCommandVerifier/IsValid.t.sol
+++ test/src/verifiers/LinkXHandleCommandVerifier/IsValid.t.sol
@@ -23,15 +23,16 @@ contract IsValidTest is Test {
     }
 
     // when verifier fails it reverts not returns false
+    // expect revert for now
     // TODO: figure this out
-    // function test_returnsFalseForInvalidProof() public view {
-    //     (LinkXHandleCommand memory command,) = LinkXHandleCommandTestFixture.getFixture();
-    //     bytes memory proof = new bytes(command.proof.length);
-    //     proof[0] = command.proof[0] ^ bytes1(uint8(1));
-    //     command.proof = proof;
-    //     bool isValid = _verifier.verify(abi.encode(command));
-    //     assertFalse(isValid);
-    // }
+    function test_revertsWhen_InvalidProof() public {
+        (LinkXHandleCommand memory command,) = LinkXHandleCommandTestFixture.getFixture();
+        bytes memory proof = new bytes(command.proof.length);
+        proof[0] = command.proof[0] ^ bytes1(uint8(1));
+        command.proof = proof;
+        vm.expectRevert();
+        _verifier.verify(abi.encode(command));
+    }
 
     function test_returnsTrueForValidCommand() public view {
         (LinkXHandleCommand memory command,) = LinkXHandleCommandTestFixture.getFixture();
