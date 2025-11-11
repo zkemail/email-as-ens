9fbfbfefb79371fc3b2ef0eeac52f2ca87975ff9
zkfriendly <zkfriendly@proton.me>
2025-11-11 21:01:00 +0100

    chore: move accounts to its own directory

--- src/accounts/MinimalAccount.sol
+++ src/accounts/MinimalAccount.sol
@@ -1,14 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
 
-import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { OwnableUpgradeable } from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
 /**
  * @title MinimalAccount
  * @notice A minimal account contract that allows for the execution of transactions with a single operator
- * @dev This contract is intended to be used as a minimal account for the ZkEmail platform
+ * @dev This contract is intended to be used as a minimal account for the ens <-> zkemail integration
  */
-contract MinimalAccount is Ownable {
+contract MinimalAccount is OwnableUpgradeable {
+    bytes32 public ensNode; // namehash(ensName)
     address public operator;
 
     event OperatorSet(address indexed operator);
@@ -23,9 +24,17 @@ contract MinimalAccount is Ownable {
         _;
     }
 
-    constructor(address owner) Ownable(owner) { }
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
 
     receive() external payable { }
+
+    function initialize(address owner, bytes32 node) external initializer {
+        __Ownable_init(owner);
+        ensNode = node;
+    }
 
     function execute(address to, uint256 value, bytes memory data) external onlyOperator returns (bytes memory) {
         (bool success, bytes memory result) = to.call{ value: value }(data);
@@ -35,5 +44,6 @@ contract MinimalAccount is Ownable {
 
     function setOperator(address newOperator) external onlyOwner {
         operator = newOperator;
+        emit OperatorSet(newOperator);
     }
 }
