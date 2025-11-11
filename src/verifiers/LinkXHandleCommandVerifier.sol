b5c52e39eb368f2dc386e1449762607f66fc2b87
zkfriendly <zkfriendly@proton.me>
2025-11-11 21:03:58 +0100

    refactor: extract core handle verification into separate reusable contract

--- src/verifiers/LinkXHandleCommandVerifier.sol
+++ src/verifiers/LinkXHandleCommandVerifier.sol
@@ -1,15 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
 
-import { Arrays } from "@openzeppelin/contracts/utils/Arrays.sol";
 import { Strings } from "@openzeppelin/contracts/utils/Strings.sol";
-import { IDKIMRegistry } from "@zk-email/contracts/interfaces/IERC7969.sol";
-import { NoirUtils } from "@zk-email/contracts/utils/NoirUtils.sol";
 import { CommandUtils } from "@zk-email/email-tx-builder/src/libraries/CommandUtils.sol";
 import { IHonkVerifier } from "../interfaces/IHonkVerifier.sol";
-import { IVerifier } from "../interfaces/IVerifier.sol";
-import { EnsUtils } from "../utils/EnsUtils.sol";
 import { TextRecord } from "../entrypoints/LinkTextRecordEntrypoint.sol";
+
+import { HandleVerifier } from "./HandleVerifier.sol";
+import { PublicInputs } from "./HandleVerifier.sol";
 
 /**
  * @notice Enum representing the indices of command parameters in the command template
@@ -20,81 +18,24 @@ enum CommandParamIndex {
     ENS_NAME
 }
 
-struct PublicInputs {
-    bytes32 pubkeyHash;
-    bytes32 emailNullifier;
-    bytes32 headerHash;
-    address proverAddress;
-    string command;
-    string xHandle;
-    string senderDomain;
-}
-
 struct LinkXHandleCommand {
     TextRecord textRecord;
     bytes proof;
     PublicInputs publicInputs;
 }
 
-contract LinkXHandleCommandVerifier is IVerifier {
-    using Arrays for bytes32[];
-
-    // #1: pubkey_hash -> 1 field -> idx 0
-    uint256 public constant PUBKEY_HASH_OFFSET = 0;
-    // #2: email_nullifier -> 1 field -> idx 1
-    uint256 public constant EMAIL_NULLIFIER_OFFSET = 1;
-    uint256 public constant EMAIL_NULLIFIER_NUM_FIELDS = 1;
-    // #3: header_hash -> 2 fields -> idx 2-3
-    uint256 public constant HEADER_HASH_OFFSET = 2;
-    uint256 public constant HEADER_HASH_NUM_FIELDS = 2;
-    // #4: prover_address -> 1 field -> idx 4
-    uint256 public constant PROVER_ADDRESS_OFFSET = 4;
-    uint256 public constant PROVER_ADDRESS_NUM_FIELDS = 1;
-    // #5: command 20 fields -> idx 5-24 (605 bytes)
-    uint256 public constant COMMAND_OFFSET = 5;
-    uint256 public constant COMMAND_NUM_FIELDS = 20;
-    // #6: x_handle_capture_1 64 fields + 1 field (length) = 65 fields -> idx 25-89
-    uint256 public constant X_HANDLE_OFFSET = 25;
-    uint256 public constant X_HANDLE_NUM_FIELDS = 65;
-    // #7: sender_domain_capture_1 64 fields + 1 field (length) -> idx 90-154
-    uint256 public constant SENDER_DOMAIN_OFFSET = 90;
-    uint256 public constant SENDER_DOMAIN_NUM_FIELDS = 65;
-
-    uint256 public constant PUBLIC_INPUTS_LENGTH = 155;
-
-    address public immutable HONK_VERIFIER;
-    address public immutable DKIM_REGISTRY;
-
-    error InvalidPublicInputsLength();
-
-    error InvalidDkimKeyHash();
-
-    modifier onlyValidDkimKeyHash(string memory domainName, bytes32 dkimKeyHash) {
-        if (!_isValidDkimKeyHash(domainName, dkimKeyHash)) revert InvalidDkimKeyHash();
-        _;
-    }
-
-    constructor(address honkVerifier, address dkimRegistry) {
-        HONK_VERIFIER = honkVerifier;
-        DKIM_REGISTRY = dkimRegistry;
-    }
+contract LinkXHandleCommandVerifier is HandleVerifier {
+    constructor(address honkVerifier, address dkimRegistry) HandleVerifier(honkVerifier, dkimRegistry) { }
 
     /**
-     * @inheritdoc IVerifier
+     * @inheritdoc HandleVerifier
      */
-    function dkimRegistryAddress() external view returns (address) {
-        return DKIM_REGISTRY;
-    }
-
-    /**
-     * @inheritdoc IVerifier
-     */
-    function verify(bytes memory data) external view returns (bool) {
+    function verify(bytes memory data) external view override returns (bool) {
         return _isValid(abi.decode(data, (LinkXHandleCommand)));
     }
 
     /**
-     * @inheritdoc IVerifier
+     * @inheritdoc HandleVerifier
      */
     function encode(
         bytes calldata proof,
@@ -102,14 +43,10 @@ contract LinkXHandleCommandVerifier is IVerifier {
     )
         external
         pure
+        override
         returns (bytes memory encodedCommand)
     {
         return abi.encode(_buildLinkXHandleCommand(proof, publicInputs));
-    }
-
-    function _isValidDkimKeyHash(string memory domainName, bytes32 dkimKeyHash) internal view returns (bool) {
-        bytes32 domainHash = keccak256(bytes(domainName));
-        return IDKIMRegistry(DKIM_REGISTRY).isKeyHashValid(domainHash, dkimKeyHash);
     }
 
     function _isValid(LinkXHandleCommand memory command)
@@ -123,58 +60,6 @@ contract LinkXHandleCommandVerifier is IVerifier {
         return IHonkVerifier(HONK_VERIFIER).verify(command.proof, _packPublicInputs(publicInputs))
             && Strings.equal(command.textRecord.value, publicInputs.xHandle)
             && Strings.equal(_getCommand(command), publicInputs.command);
-    }
-
-    function _packPublicInputs(PublicInputs memory publicInputs) internal pure returns (bytes32[] memory fields) {
-        fields = new bytes32[](PUBLIC_INPUTS_LENGTH);
-        fields[PUBKEY_HASH_OFFSET] = publicInputs.pubkeyHash;
-        fields[EMAIL_NULLIFIER_OFFSET] = publicInputs.emailNullifier;
-        _copyTo(fields, HEADER_HASH_OFFSET, EnsUtils.packHeaderHash(publicInputs.headerHash));
-        _copyTo(
-            fields,
-            PROVER_ADDRESS_OFFSET,
-            NoirUtils.packFieldsArray(abi.encodePacked(publicInputs.proverAddress), PROVER_ADDRESS_NUM_FIELDS)
-        );
-        _copyTo(fields, COMMAND_OFFSET, NoirUtils.packFieldsArray(bytes(publicInputs.command), COMMAND_NUM_FIELDS));
-        _copyTo(fields, X_HANDLE_OFFSET, NoirUtils.packBoundedVecU8(bytes(publicInputs.xHandle), X_HANDLE_NUM_FIELDS));
-        _copyTo(
-            fields,
-            SENDER_DOMAIN_OFFSET,
-            NoirUtils.packBoundedVecU8(bytes(publicInputs.senderDomain), SENDER_DOMAIN_NUM_FIELDS)
-        );
-        return fields;
-    }
-
-    function _unpackPublicInputs(bytes32[] calldata fields) internal pure returns (PublicInputs memory publicInputs) {
-        if (fields.length != PUBLIC_INPUTS_LENGTH) revert InvalidPublicInputsLength();
-
-        return PublicInputs({
-            pubkeyHash: fields[PUBKEY_HASH_OFFSET],
-            emailNullifier: fields[EMAIL_NULLIFIER_OFFSET],
-            headerHash: EnsUtils.unpackHeaderHash(
-                fields.slice(HEADER_HASH_OFFSET, HEADER_HASH_OFFSET + HEADER_HASH_NUM_FIELDS)
-            ),
-            proverAddress: address(
-                uint160(
-                    bytes20(
-                        NoirUtils.unpackFieldsArray(
-                            fields.slice(PROVER_ADDRESS_OFFSET, PROVER_ADDRESS_OFFSET + PROVER_ADDRESS_NUM_FIELDS)
-                        )
-                    )
-                )
-            ),
-            command: string(
-                NoirUtils.unpackFieldsArray(fields.slice(COMMAND_OFFSET, COMMAND_OFFSET + COMMAND_NUM_FIELDS))
-            ),
-            xHandle: string(
-                NoirUtils.unpackBoundedVecU8(fields.slice(X_HANDLE_OFFSET, X_HANDLE_OFFSET + X_HANDLE_NUM_FIELDS))
-            ),
-            senderDomain: string(
-                NoirUtils.unpackBoundedVecU8(
-                    fields.slice(SENDER_DOMAIN_OFFSET, SENDER_DOMAIN_OFFSET + SENDER_DOMAIN_NUM_FIELDS)
-                )
-            )
-        });
     }
 
     function _buildLinkXHandleCommand(
@@ -221,12 +106,5 @@ contract LinkXHandleCommandVerifier is IVerifier {
         template[5] = CommandUtils.STRING_MATCHER;
 
         return template;
-    }
-
-    function _copyTo(bytes32[] memory fields, uint256 offset, bytes32[] memory data) private pure {
-        // solhint-disable-next-line no-inline-assembly
-        assembly ("memory-safe") {
-            mcopy(add(add(0x20, fields), mul(offset, 0x20)), add(0x20, data), mul(mload(data), 0x20))
-        }
     }
 }
